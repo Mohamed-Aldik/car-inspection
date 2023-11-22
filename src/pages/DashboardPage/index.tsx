2de2a98edec196991958db050f3d84c75d51f285
mac <mohamed@fanitrade.com>
2023-11-22 20:51:38 +0400

    create DashboardPage component

--- src/pages/DashboardPage/index.tsx
+++ src/pages/DashboardPage/index.tsx
@@ -1,5 +1,29 @@
+import { Box, Button, Text } from "@chakra-ui/react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../redux/store";
+import { logOut } from "../../redux/slices/userSlice";
+import { useNavigate } from "react-router-dom";
+
 const DashboardPage = () => {
-  return <>DashboardPage</>;
+  const userName = useSelector((state: RootState) => state.user.user?.name);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(logOut());
+    localStorage.removeItem("loggedInUser");
+    navigate("/");
+  };
+
+  return (
+    <Box p={4}>
+      <Text fontSize="xl">Welcome to your Dashboard, {userName}</Text>
+      {/* Dashboard content goes here */}
+      <Button colorScheme="red" mt={4} onClick={handleLogout}>
+        Logout
+      </Button>
+    </Box>
+  );
 };
 
 export default DashboardPage;
