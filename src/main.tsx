aab044ce1c1ffc510bf850c1629928507cdc807a
mac <mohamed@fanitrade.com>
2023-11-23 01:24:46 +0400

    feat(pages): create FullPageSpinner component

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "./redux/store";
+import FullPageSpinner from "./pages/FullPageSpinner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ReduxProvider store={store}>
-        <ChakraProvider>
-          <App />
-        </ChakraProvider>
-      </ReduxProvider>
-    </BrowserRouter>
+    <Suspense fallback={<FullPageSpinner />}>
+      <BrowserRouter>
+        <ReduxProvider store={store}>
+          <ChakraProvider>
+            <App />
+          </ChakraProvider>
+        </ReduxProvider>
+      </BrowserRouter>
+    </Suspense>
   </React.StrictMode>
 );
---
+++ src/pages/FullPageSpinner/index.tsx
@@ -0,0 +1,7 @@
+import { Text } from "@chakra-ui/react";
+
+const FullPageSpinner = () => {
+  return <Text>Loading...</Text>;
+};
+
+export default FullPageSpinner;
