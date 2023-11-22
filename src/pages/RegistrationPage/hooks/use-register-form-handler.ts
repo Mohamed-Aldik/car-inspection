d6197cc2629e4348cfca0a8c302600d68cfb549e
mac <mohamed@fanitrade.com>
2023-11-22 20:24:59 +0400

    create RegistrationPage component

---
+++ src/pages/RegistrationPage/hooks/use-register-form-handler.ts
@@ -0,0 +1,86 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useNavigate } from "react-router-dom";
+import { useToast } from "@chakra-ui/react";
+import { User } from "../../../types/user";
+
+// Validation Schema
+const schema = yup.object().shape({
+    name: yup.string().required("Name is required"),
+    email: yup
+      .string()
+      .email("Invalid email format")
+      .required("Email is required"),
+    phoneNumber: yup
+      .string()
+      .matches(/^(\+\d{1,3}[- ]?)?\d{4,10}$/, "Invalid phone number") // Changed the regex to allow 5 to 12 digits
+      .required("Phone number is required"),
+    password: yup
+      .string()
+      .min(6, "Password must be at least 6 characters")
+      .required("Password is required"),
+    confirmPassword: yup
+      .string()
+      .oneOf([yup.ref("password"), undefined], "Passwords must match")
+      .required("Confirm password is required"),
+  });
+
+  export const useRegisterFormHandler =()=>{
+    const navigate = useNavigate();
+    const toast = useToast();
+  
+  
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+      } = useForm({
+        resolver: yupResolver(schema),
+        mode: "onChange",
+      });
+    
+      const onSubmit = (data: User) => {
+        const users = JSON.parse(localStorage.getItem("users") || "[]");
+
+        if (users.some((user: User) => user.email === data.email)) {
+          toast({
+            title: "Registration failed.",
+            description: "Email already registered.",
+            status: "error",
+            duration: 2000,
+            isClosable: true,
+            position: "top-right",
+          });
+          return;
+        }
+    
+        const newUser = {
+          name: data.name,
+          email: data.email,
+          phoneNumber: data.phoneNumber,
+          password: data.password, // In a real application, password should be hashed
+        };
+    
+        users.push(newUser);
+        localStorage.setItem("users", JSON.stringify(users));
+    
+        toast({
+          title: "Registration successful.",
+          description: "You have successfully registered!",
+          status: "success",
+          duration: 2000,
+          isClosable: true,
+          position: "top-right",
+        });
+    
+        navigate("/login");
+      };
+
+      return {
+        errors,
+        handleSubmit,
+        register,
+        onSubmit,
+      };
+  }
--- src/pages/RegistrationPage/index.tsx
+++ src/pages/RegistrationPage/index.tsx
@@ -1,5 +1,57 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  FormErrorMessage,
+} from "@chakra-ui/react";
+import { useRegisterFormHandler } from "./hooks/use-register-form-handler";
+
 const RegistrationPage = () => {
-  return <>RegistrationPage</>;
+  const { errors, handleSubmit, register, onSubmit } = useRegisterFormHandler();
+
+  return (
+    <Box as={"form"} p={4} onSubmit={handleSubmit(onSubmit)}>
+      <FormControl isInvalid={!!errors.name}>
+        <FormLabel htmlFor="name">Name</FormLabel>
+        <Input id="name" {...register("name")} />
+        <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
+      </FormControl>
+
+      <FormControl isInvalid={!!errors.email} mt={4}>
+        <FormLabel htmlFor="email">Email</FormLabel>
+        <Input id="email" type="email" {...register("email")} />
+        <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+      </FormControl>
+
+      <FormControl isInvalid={!!errors.phoneNumber} mt={4}>
+        <FormLabel htmlFor="phoneNumber">Phone Number</FormLabel>
+        <Input id="phoneNumber" {...register("phoneNumber")} />
+        <FormErrorMessage>{errors.phoneNumber?.message}</FormErrorMessage>
+      </FormControl>
+
+      <FormControl isInvalid={!!errors.password} mt={4}>
+        <FormLabel htmlFor="password">Password</FormLabel>
+        <Input id="password" type="password" {...register("password")} />
+        <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+      </FormControl>
+
+      <FormControl isInvalid={!!errors.confirmPassword} mt={4}>
+        <FormLabel htmlFor="confirmPassword">Confirm Password</FormLabel>
+        <Input
+          id="confirmPassword"
+          type="password"
+          {...register("confirmPassword")}
+        />
+        <FormErrorMessage>{errors.confirmPassword?.message}</FormErrorMessage>
+      </FormControl>
+
+      <Button mt={4} colorScheme="red" type="submit">
+        Register
+      </Button>
+    </Box>
+  );
 };
 
 export default RegistrationPage;
