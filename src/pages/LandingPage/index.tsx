6001fcea37264a9a5ea1a055919bfbe714e9047c
mac <mohamed@fanitrade.com>
2023-11-22 20:00:15 +0400

    create LandingPage component

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,5 +1,18 @@
+import { Box, Button, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
 const LandingPage = () => {
-  return <>LandingPage</>;
+  return (
+    <Box textAlign="center" py={10}>
+      <Text fontSize="xl">Welcome to the Car Inspection Portal</Text>
+      <Button colorScheme="red" mt={4} as={Link} to="/register">
+        Register
+      </Button>
+      <Button colorScheme="red" mt={4} ml={4} as={Link} to="/login">
+        Login
+      </Button>
+    </Box>
+  );
 };
 
 export default LandingPage;
