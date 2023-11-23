2cf867df037f34e108865b9653fab2251d48cf47
mac <mohamed@fanitrade.com>
2023-11-23 16:35:16 +0400

    Refactor(404-page): Enhance the user interface for the 404 page

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,10 +1,45 @@
-import { Text } from "@chakra-ui/react";
+import { Button, Container, Heading, Text, VStack } from "@chakra-ui/react";
 import { Layout } from "../../layout";
+import { Link } from "react-router-dom";
+import { URL } from "../../utils/constants";
 
 const NotFoundPage = () => {
   return (
     <Layout>
-      <Text>Page Not Found</Text>
+      <Container
+        as={VStack}
+        align={"center"}
+        justify={"center"}
+        minH="calc(100vh - 140px)"
+      >
+        <Heading
+          display="inline-block"
+          as="h2"
+          size="2xl"
+          bgGradient="linear(to-r, red.400, red.600)"
+          backgroundClip="text"
+        >
+          404
+        </Heading>
+        <Text fontSize="18px" mt={3} mb={2}>
+          Page Not Found
+        </Text>
+        <Text color={"gray.500"} mb={6}>
+          The page you&apos;re looking for does not seem to exist
+        </Text>
+
+        <Button
+          w={"sm"}
+          as={Link}
+          to={URL.LANDING_PAGE}
+          colorScheme="red"
+          bgGradient="linear(to-r, red.400, red.500, red.600)"
+          color="white"
+          variant="solid"
+        >
+          Go to Home
+        </Button>
+      </Container>
     </Layout>
   );
 };
