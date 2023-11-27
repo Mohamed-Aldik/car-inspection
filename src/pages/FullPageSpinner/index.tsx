706a393ea7b602ef9f852c027c6dfde472210f33
mac <mohamed@fanitrade.com>
2023-11-27 11:22:22 +0400

    feat(index): implement FullPageSpinner as Suspense fallback

--- src/pages/FullPageSpinner/index.tsx
+++ src/pages/FullPageSpinner/index.tsx
@@ -1,7 +1,11 @@
-import { Text } from "@chakra-ui/react";
+import { Spinner, VStack } from '@chakra-ui/react';
 
 const FullPageSpinner = () => {
-  return <Text>Loading...</Text>;
+  return (
+    <VStack bg={'gray.50'} align={'center'} justify={'center'} h='100vh'>
+      <Spinner size='xl' color='red.500' />
+    </VStack>
+  );
 };
 
 export default FullPageSpinner;
